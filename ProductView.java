508850bafa3a1e97f34a623452d3e9b0879c3a1e
Tawhid Islam <79523960+fmtawhid@users.noreply.github.com>
2023-06-12 21:57:44 +0600

    Create ProductView.java file

---
+++ ProductView.java
@@ -0,0 +1,27 @@
+package firstproject;
+
+/**
+ *
+ * @author Tawhid Islam
+ */
+public class FirstProject {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int id, price;
+        String title, disc, cata;
+        id = 001;
+        title = "iPhone 15";
+        price =9999;
+        disc = "New Collection in the Apple Park";
+        cata = " Phone";
+        
+        System.out.println("Welcome To Out 360 digri Product View");
+        System.out.println("Id number: " + id );
+        System.out.println("Title: " + title );
+        System.out.println("Discribe: " + disc );
+        System.out.println("Catagory: " + cata );
+        System.out.println("Price Value: " + price + " USD");           
+    }
+}
