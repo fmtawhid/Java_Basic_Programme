422bba14881dca8ef52ca3a19489944ad1160ca8
Tawhid Islam <79523960+fmtawhid@users.noreply.github.com>
2023-06-12 22:18:56 +0600

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,11 @@
+
+package firstproject;
+import java.util.Scanner;
+public class FirstProject {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);   
+        System.out.print("Enter the Number: ");
+        int number = input.nextInt();  
+        System.out.println("Number is = " + number);    
+    } 
+}
